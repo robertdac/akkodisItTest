605fb05c5e70a8cfa3f95b8dbe9c2da6c3a5a214
Robert <robert.deabreu@borrox.com>
2025-06-30 08:55:31 +0200

    add new routes api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,36 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\Api\V1\AuthController;
+    use App\Http\Controllers\ShiftAssignmentController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "api" middleware group. Make something great!
+    |
+    */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('v1')->group(function() {
+        Route::post('/login', [AuthController::class, 'login']);
+
+        Route::middleware('auth:sanctum')->group(function() {
+            Route::get('/me', [AuthController::class, 'me']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
+
+
+        Route::middleware(['auth:sanctum', 'isAdmin'])
+            ->get('/users/{user}/shifts', [ShiftAssignmentController::class, 'show'])
+            ->name('users.shifts.show');
+
+    });
