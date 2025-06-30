fbbfef301b1d79a8650711b83429a77dd19fe26c
Robert <robert.deabreu@borrox.com>
2025-06-30 09:00:26 +0200

    Add las_name user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,12 +25,14 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
